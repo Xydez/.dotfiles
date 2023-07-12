50dd4172d4bc1b1209f712150726ae7516e03b46
Johannes Lager <johannes.lager@outofhome.se>
2023-07-12 12:49:56 +0200

    fix shit and remove lockfiles and stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,18 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    abbr --add gs 'git status'
-    abbr --add gd 'git diff'
     abbr --add ga 'git add'
-    abbr --add gr 'git restore --staged'
     abbr --add gc 'git commit'
     abbr --add gco 'git checkout'
+    abbr --add gd 'git diff'
+    abbr --add gds 'git diff --staged'
     abbr --add gp 'git push'
+    abbr --add gs 'git status'
+    abbr --add gst 'git stash'
+    abbr --add gu 'git restore --staged'
+
+    export EDITOR=nvim
+
+    if not set -q TMUX
+        exec tmux
+    end
 end
 
