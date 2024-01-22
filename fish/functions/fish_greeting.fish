c630ff347c5856d0e1ccd58594979e9c2823adaf
Johannes <thexydez@gmail.com>
2024-01-22 22:13:10 +0100

    Add mako, binds and better greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,4 +1,5 @@
 function fish_greeting
+	#test (cat /sys/class/power_supply/BAT0/status) = 'Charging'
     echo
     echo -e "\033[0;31m---{ Välkommen till Johannes dator }---\033[0m"
     echo -e "\033[0;31m       Klockan är $(date '+%T') \033[0m"
