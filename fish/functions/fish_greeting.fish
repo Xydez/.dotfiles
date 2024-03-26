0b8bc4b8daf9682c6d8f5a12fbfdec053e9e8354
Johannes <thexydez@gmail.com>
2024-03-26 14:09:03 +0100

    Wayland fixes

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -2,6 +2,7 @@ function fish_greeting
 	#test (cat /sys/class/power_supply/BAT0/status) = 'Charging'
     echo
     echo -e "\033[0;31m---{ Välkommen till Johannes dator }---\033[0m"
+	echo -e "\033[0;31m       Datumet är $(date '+%F') \033[0m"
     echo -e "\033[0;31m       Klockan är $(date '+%T') \033[0m"
     echo -e "\033[0;31m       Batterinivå $(cat /sys/class/power_supply/BAT0/capacity)%\033[0m"
     echo
