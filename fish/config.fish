02ec63462637884e5594646c44b2f61427a25cc1
Johannes <thexydez@gmail.com>
2025-11-13 15:01:57 +0100

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
     abbr --add ga 'git add'
     abbr --add gc 'git commit'
+	abbr --add gca 'git commit --amend --no-edit'
     abbr --add gco 'git checkout'
     abbr --add gd 'git diff'
     abbr --add gds 'git diff --staged'
     abbr --add gp 'git push'
+	abbr --add gpf 'git push --force-with-lease'
     abbr --add gpm 'git emma'
     abbr --add gs 'git status'
     abbr --add gst 'git stash'
@@ -27,6 +29,13 @@ if status is-login
         exec Hyprland
     end
 end
+
+# See the following:
+#   Systemd users can also start Hyprland, using uwsm. This is the recommended method of launching
+#   Hyprland on systemd-based distros, as it provides additional features such as xdg-autostart
+#   support, launching any application as a systemd-unit with uwsm app helper, and the ability to
+#   enable services for programs that rely on a graphical session and provide such services (e.g
+#   waybar). See uwsm page for further instructions.
 
 bind \cs forward-char
 
--- nvim/lua/johannes/opt.lua
+++ nvim/lua/johannes/opt.lua
@@ -2,7 +2,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop     = 4 -- Width of tab character
-vim.opt.softtabstop = 4
+-- vim.opt.softtabstop = 4
 vim.opt.shiftwidth  = 4
 vim.opt.expandtab = true
 
